11f9a90c1e8d2ad416e9704f085215ad41bd732b
Erick Alexander <erickmg_13@hotmail.com>
2021-10-24 06:19:23 -0500

    prueba

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::get('/dashboard', [WebController::class, 'dashboard'])->name('web.dashboard');
